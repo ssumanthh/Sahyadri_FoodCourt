2d9bde45e6067e744c2f8f4c185c34d4c8748c04
Sumanth prabhu <44114195+ssumanthh@users.noreply.github.com>
2021-07-28 12:41:30 +0530

    Update foodcard.dart

--- lib/widgets/foodcard.dart
+++ lib/widgets/foodcard.dart
@@ -3,10 +3,15 @@ import 'package:sahyadri_food_court/widgets/food_details.dart';
 import '../authentication/auth.dart';
 import 'package:flutter/rendering.dart';
 
-class Foodcard extends StatefulWidget{
-  Foodcard({
-    required this.auth,required this.fd, required this.img,required this.name, required this.price,required this.available,required this.added
-  });
+class Foodcard extends StatefulWidget {
+  Foodcard(
+      {required this.auth,
+      required this.fd,
+      required this.img,
+      required this.name,
+      required this.price,
+      required this.available,
+      required this.added});
   final BaseAuth auth;
   final String fd;
   final String img;
@@ -15,91 +20,138 @@ class Foodcard extends StatefulWidget{
   final int available;
   final bool added;
   @override
-  _FoodcardState createState()=>_FoodcardState();
+  _FoodcardState createState() => _FoodcardState();
 }
 
-class _FoodcardState extends State<Foodcard>{
+class _FoodcardState extends State<Foodcard> {
+  bool click = false;
   @override
   Widget build(BuildContext context) {
-      bool fav = false;
-      bool notFav = false;
-  return InkWell( 
-    onTap: (){
-      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Food_Details(
-                            auth:widget.auth,foodId: widget.fd, img:widget.img, name:widget.name, price:widget.price,available:widget.available, added: true,
-                              ),
-                        ),
-                      );
-    },
-    child:Container(
-    child: Card(
-      child: Padding(
-        padding: const EdgeInsets.all(5.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.network(
-              widget.img,
-              height: 55.0,
-            ),
-            Text(
-              widget.name,
-              style: TextStyle(
-                fontSize: 22.0,
-                fontWeight: FontWeight.w600,
+   
+    return InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => Food_Details(
+                auth: widget.auth,
+                foodId: widget.fd,
+                img: widget.img,
+                name: widget.name,
+                price: widget.price,
+                available: widget.available,
+                added: true,
               ),
             ),
-            // SizedBox(
-            //   height: 2.0,
-            // ),
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    "${widget.price} ₹",
+          );
+        },
+        child: Container(
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.network(
+                    widget.img,
+                    height: 55.0,
+                    errorBuilder: (BuildContext context, Object exception,
+                        StackTrace? stackTrace) {
+                      // Appropriate logging or analytics, e.g.
+                      // myAnalytics.recordError(
+                      //   'An error occurred loading "https://example.does.not.exist/image.jpg"',
+                      //   exception,
+                      //   stackTrace,
+                      // );
+                      return const Opacity(
+                          opacity: 0.8, child: Icon(Icons.image));
+                    },
+                    loadingBuilder: (BuildContext context, Widget child,
+                        ImageChunkEvent? loadingProgress) {
+                      if (loadingProgress == null) {
+                        return child;
+                      }
+                      return Opacity(opacity: 0.4, child: Icon(Icons.image));
+                      // CircularProgressIndicator(
+                      //     value: loadingProgress.expectedTotalBytes != null
+                      //         ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
+                      //         : null,
+
+                      // );
+                    },
+                  ),
+                  Text(
+                    widget.name,
                     style: TextStyle(
                       fontSize: 22.0,
-                      fontWeight: FontWeight.w700,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
-                ),
-                Expanded(
-                  child: widget.added
-                      ? IconButton(
-                          onPressed: () {
-                            widget.auth.deleteUserFav(widget.name);
-                            notFav = true;
-                          },
-                          icon:!notFav? Icon(
-                            Icons.favorite,
-                            color: Colors.red,
-                          ):Icon(
-                            Icons.favorite,
-                            color: Colors.black,
-                          )
-                          )
-                      : IconButton(
-                          onPressed: () {
-                            widget.auth.addUserFav(widget.name,'i',3);
-                          },
-                          icon: Icon(
-                            Icons.favorite_outline_rounded,
-                            color: Colors.black,
+                  // SizedBox(
+                  //   height: 2.0,
+                  // ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          "${widget.price} ₹",
+                          style: TextStyle(
+                            fontSize: 22.0,
+                            fontWeight: FontWeight.w700,
                           ),
                         ),
-                )
-              ],
+                      ),
+                      Expanded(
+                        child: widget.added
+                            ? IconButton(
+                                onPressed: () {
+                                  widget.auth.deleteUserFav(widget.name);
+                                  setState(() {
+                                    click = true;
+                                  });
+                                },
+                                icon: !click
+                                    ? Icon(
+                                        Icons.favorite,
+                                        color: Colors.red,
+                                      )
+                                    :                                       Icon(
+                                  Icons.favorite_outline_rounded,
+                                  color: Colors.black,
+                                )
+                            )
+                            : IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    click = true;
+                                  });
+                                  widget.auth.addUserFav(widget.name);
+                                },
+                                icon:click?Icon(
+                                        Icons.favorite,
+                                        color: Colors.red,
+                                      ):
+                                      Icon(
+                                  Icons.favorite_outline_rounded,
+                                  color: Colors.black,
+                                ),
+                              ),
+                      )
+                    ],
+                  ),
+                  widget.available > 0
+                      ? Text(
+                          'Available....',
+                          style: TextStyle(color: Colors.green),
+                        )
+                      : Text(
+                          ' Not Available!',
+                          style: TextStyle(color: Colors.red),
+                        ),
+                ],
+              ),
             ),
-            widget.available>0?Text('Available....',
-            style:TextStyle(color: Colors.green),):Text(' Not Available!',
-            style:TextStyle(color: Colors.red),),
-          ],
-        ),
-      ),
-    ),
-  ));
-}
+          ),
+        ));
+  }
 }
