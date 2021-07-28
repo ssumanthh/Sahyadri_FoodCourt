d9535ab9a2d390d714aebdc6788ffa48f8d7fab8
Sumanth prabhu <44114195+ssumanthh@users.noreply.github.com>
2021-07-28 12:53:35 +0530

    drawer added

--- lib/admin/admins.dart
+++ lib/admin/admins.dart
@@ -4,7 +4,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:sahyadri_food_court/admin/admin_order.dart';
 import 'package:sahyadri_food_court/authentication/auth.dart';
+import 'package:sahyadri_food_court/widgets/about_us.dart';
 import 'package:sahyadri_food_court/widgets/foodcard.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Admin extends StatefulWidget {
   Admin({required this.auth, required this.onSignOut});
@@ -26,12 +28,17 @@ class _AdminState extends State<Admin> {
   //   "https://pngimage.net/wp-content/uploads/2018/06/sizzler-png-8.png",
   // ];
   int index = 0;
-  
+  bool search = false;
+  String? fid = '';
+  String email = '';
+  String name = '';
+  TextEditingController searchControler=TextEditingController();
+
   
   Widget _buildGride(QuerySnapshot? snapshot) {
     return ListView.builder(
         itemCount: snapshot!.docs.length,
-        itemBuilder: (context, index) {
+        itemBuilder: (context, index) { 
           final doc = snapshot.docs[index]; 
           print(doc.id);
           TextEditingController availableControler = new TextEditingController();
@@ -103,6 +110,31 @@ class _AdminState extends State<Admin> {
         });
   }
 
+  void getid() async {
+    await widget.auth.getfid().then((value) async {
+      setState(() {
+        fid = value;
+      });
+    });
+  }
+
+  void _launchURL(String url) async {
+    String _url = url;
+    await canLaunch(_url) ? await launch(_url) : throw 'Could not launch $_url';
+  }
+
+  void initState() {
+    super.initState();
+    widget.auth.currentUseremail().then((value) {
+      email = value.toString();
+    });
+    getid();
+    widget.auth.getname().then((value) {
+      name = value.toString();
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     void _signOut() async {
@@ -125,24 +157,102 @@ class _AdminState extends State<Admin> {
             color: Color(0xFFfc6a26),
           ),
         ),
-        actions: <Widget>[
-          RaisedButton(
-              child: new Text('signout'),
-              onPressed: () {
-                _signOut();
-              }),
-        ],
-        centerTitle: true,
-        elevation: 0.0,
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(
-            Icons.menu,
-            color: Colors.black,
-          ),
-        ),
+        
       ),
-
+      drawer: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(50),
+            bottomRight: Radius.circular(50),
+          ),
+          child: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(color: Color(0xFFfc6a26)),
+                  accountName: Text(name),
+                  accountEmail: Text(email),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://images.unsplash.com/photo-1480429370139-e0132c086e2a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fG1hbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.person,
+                    color: Color(0xFFfc6a26),
+                  ),
+                  title: Text("Account"),
+                  subtitle: Text("Faculty Id:$fid"),
+                  trailing: Icon(Icons.edit),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Color(0xFFfc6a26),
+                  ),
+                  title: Text("Email"),
+                  subtitle: Text(email),
+                  trailing: Icon(Icons.send),
+                  onTap: () {
+                    final Uri params = Uri(
+                                        scheme: 'mailto',
+                                        path:
+                                            'sumanth.is18@sahadri.edu.in',
+                                      );
+                                      _launchURL(params.toString());
+                  },
+                ),
+                ListTile(
+                    title: new Row(children: [
+                      Icon(
+                        Icons.info_outline,
+                        color: Color(0xFFfc6a26),
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+                        child: Text(
+                          'About Us',
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                      )
+                    ]),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => AbooutUs()),
+                      );
+                    }),
+                ListTile(
+                  title: new Row(children: [
+                    Icon(
+                      Icons.exit_to_app,
+                      color: Color(0xFFfc6a26),
+                    ),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+                      child: Text(
+                        'logOut',
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ]),
+                  onTap: () {
+                    _signOut();
+                  },
+                ),
+              ],
+            ),
+          )),
       //Now let's build the body of our app
       body: index==0? Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
@@ -172,6 +282,18 @@ class _AdminState extends State<Admin> {
                 children: [
                   Expanded(
                       child: TextField(
+                        onChanged: (value) {
+                              if (value.isNotEmpty) {
+                                setState(() {
+                                  search = true;
+                                });
+                              } else {
+                                setState(() {
+                                  search = false;
+                                });
+                              }
+                            },
+                            controller: searchControler,
                     decoration: InputDecoration(
                       hintText: "Search... ",
                       border: InputBorder.none,
@@ -183,7 +305,11 @@ class _AdminState extends State<Admin> {
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(30.0),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                       setState(() {
+                                search = true;
+                              });
+                    },
                     child: Padding(
                       padding: const EdgeInsets.symmetric(vertical: 15.0),
                       child: Icon(
@@ -198,7 +324,23 @@ class _AdminState extends State<Admin> {
             ),
             //build the food menu
             //I'm going to create a custom widget
-            StreamBuilder<QuerySnapshot>(
+            search?StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection("food")
+                    .where(
+                      'name',
+                      isGreaterThanOrEqualTo: searchControler.text,
+                    )
+                    .where(
+                      'name', isLessThan: searchControler.text+'z')
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  print(snapshot.data);
+                  if (!snapshot.hasData) return LinearProgressIndicator();
+                  if (snapshot.hasError) return CircularProgressIndicator();
+                  return  Expanded(child:_buildGride(snapshot.data));
+                },
+              ):StreamBuilder<QuerySnapshot>(
               stream: FirebaseFirestore.instance.collection("food").snapshots(),
               builder: (context, snapshot) {
                 print(snapshot.data);
